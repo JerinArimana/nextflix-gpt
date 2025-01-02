4c14b88539de342505d6e8094ebc2a755f617e7a
jerinexpansionjs <jerin.jose@expansionjs.com>
2025-01-02 05:47:00 +0530

    learn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import Body from "./components/Body";
-
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-800">
-      <Body />
-    </div>
-  );
+  return <div className="text-3xl font-bold text-green-800">asdf</div>;
 }
 
 export default App;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,7 @@
-import Browse from "./Browse";
-import Login from "./login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 const Body = () => {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login />,
-    },
-    {
-      path: "/browse",
-      element: <Browse />,
-    },
-  ]);
   return (
     <>
-      <div>
-        <RouterProvider router={appRouter} />
-      </div>
+      <div></div>
     </>
   );
 };
